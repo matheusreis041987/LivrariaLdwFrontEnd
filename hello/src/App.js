69f70abed4c091edbecea6b3cd43117b2ec46886
matheusreis041987 <matheusgithub1987@gmail.com>
2023-07-12 21:09:25 -0300

    teste2

--- hello/src/App.js
+++ hello/src/App.js
@@ -15,10 +15,11 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         APP Teste
         </a>
       </header>
     </div>
+
   );
 }
 
